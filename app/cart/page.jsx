9d7c823f56256fe67c188d087704357e80880b07
Uday932 <uday.bhavsar.sio@gmail.com>
2024-11-06 10:43:20 +0100

    login & cart v1

---
+++ app/cart/page.jsx
@@ -0,0 +1,9 @@
+export default function Cart() {
+    return (
+        <div>
+            <h2>Mon Panier</h2>
+            <p>Aucun article dans le panier.</p>
+            {/*  */}
+        </div>
+    );
+}
